945e3418908dc434d1cd3b4738aeb52948e37a16
gado <test@tesc.om>
2017-06-03 13:15:16 +0300

    initial commit2

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
-Vue.config.productionTip = false
+Vue.config.productionTi p = false
 
 /* eslint-disable no-new */
 new Vue({
